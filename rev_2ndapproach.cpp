ba6a5edfc897437b08de90628512e1e06ff9dcaa
PRANEET-C <156356786+PRANEET-C@users.noreply.github.com>
2024-10-29 12:44:09 +0530

    Update rev_2ndapproach.cpp

--- rev_2ndapproach.cpp
+++ rev_2ndapproach.cpp
@@ -8,7 +8,7 @@ int main(){
     for(int i=0;i<n;i++){
         cin>>arr[i];
     }
-    for(int i=0;i<=n/2;i++){
+    for(int i=0;i<n/2;i++){
         int temp=arr[i];
         arr[i]=arr[n-i-1];
         arr[n-i-1]=temp;
